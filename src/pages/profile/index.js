38a8b8bd500d27760730b329cdbd6f552aac9d4d
Lazaro delas <lazarorytson1@gmail.com>
2023-04-12 21:40:43 -0300

    versao_03

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,13 +1,62 @@
 import { useContext } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View , StyleSheet, Image} from "react-native";
 import AuthContext from "../../routes/auth_context";
+
+import PrivateIcon from "../../../assets/profile/privateIcon.svg";
+import AccountList from "../../../assets/profile/AccountsList.svg";
+import User from "../../../assets/profile/User.svg";
+import Menu from "../../../assets/profile/Menu.svg";
 
 
 export default function Profile() {
 
-    const { signOut } = useContext(AuthContext);
+    return <View style = {styles.container}> 
+                <View style = {styles.container_priv}>
+                    <PrivateIcon style={{marginRight:7.5}}/>
+                    <Text style = {styles.text_principal}>jacob_w</Text>
+                    <AccountList style={{marginLeft:6}}/>
+                    <Menu/>
+                </View>
 
-    return <View style={{ backgroundColor: 'white' }}>
-        <Button onPress={() => { signOut() }} title={"Logout"} />
-    </View>
+                <View>
+                    <User/>
+                    <View>
+                        <Text>54</Text>
+                        <Text>Posts</Text>
+                    </View>
+                    
+                </View>
+
+            </View>
+
+
+
+    // const { signOut } = useContext(AuthContext);
+
+    // return <View style={{ backgroundColor: 'white' }}>
+    //     <Button onPress={() => { signOut() }} title={"Logout"} />
+    // </View>
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent:''
+    },
+
+    container_priv:{
+        flexDirection:'row',
+        marginTop:30,
+        alignItems:'center',
+    },
+
+    text_principal: {
+        fontWeight:'bold',
+        fontSize:20,
+      },
+
+  });
+  
